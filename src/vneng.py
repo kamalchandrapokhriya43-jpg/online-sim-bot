49ef9dcac3f5f8ebd5014a7e67a34f4d39ebce4b
Kamal Pokhriya <kamalchandrapokhriya43@gmail.com>
2026-01-05 08:52:51 +0530

    src/vneng.py

---
+++ src/vneng.py
@@ -0,0 +1,9 @@
+class VNEngine:
+    def get_online_countries(self):
+        return []
+
+    def get_country_numbers(self, country):
+        return []
+
+    def get_number_inbox(self, country, number):
+        return []
